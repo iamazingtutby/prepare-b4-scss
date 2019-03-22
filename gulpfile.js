5b1fdea2296100c4fa06356ddf9b174c283e1e37
Vadimjunior <vadiminsk90@gmail.com>
2019-03-22 17:23:11 +0300

    Add browser-sync. Set up html include

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require("gulp-concat");
-var minifyCss = require("gulp-minify-css");
-var uglify = require("gulp-uglify");
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var concat       = require("gulp-concat");
+var minifyCss    = require("gulp-minify-css");
+var uglify       = require("gulp-uglify");
 var autoprefixer = require('gulp-autoprefixer');
-var webserver = require('gulp-webserver');
+var webserver    = require('gulp-webserver');
+var browserSync  = require('browser-sync').create();
+var fileinclude  = require('gulp-file-include');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -25,15 +27,19 @@ gulp.task('js', function() {
         .pipe(gulp.dest("app/js"))
 });
 
-gulp.task('webserver', gulp.series('sass', function() {
-  gulp.src('app')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: false,
-      fallback: 'index.html'
-    }));
+gulp.task('fileinclude', function() {
+  gulp.src(['**/*.html'])
+      .pipe(fileinclude())
+      .pipe(gulp.dest('./'));
+});
+
+gulp.task('browser-sync', gulp.series('sass', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'app/scss/*.scss', 'app/scss/components/*.scss', 'app/scss/components/*/*.scss'], gulp.series('sass'));
 }));
 
-gulp.task('default', gulp.parallel('js', 'webserver'));
+gulp.task('default', gulp.parallel('js', 'browser-sync','fileinclude'));
